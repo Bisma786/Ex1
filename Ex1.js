432a383cad52d628bfc6c0ce80756d8e96441abb
Bisma786 <159830145+Bisma786@users.noreply.github.com>
2024-02-26 17:22:31 +0500

    Initial commit

---
+++ Ex1.js
@@ -0,0 +1,2 @@
+console.log("Hellow! Sir Zia, I installed everything you asked for.");
+//Successfully installed Vs code, Node Js, Typescript and Git.
---
+++ Ex1.ts
@@ -0,0 +1,3 @@
+console.log("Hellow! Sir Zia, I installed everything you asked for.")
+//Install Node.js, TypeScript and VS Code on your computer.
+//Successfully installed Vs code, Node Js, Typescript and Git.
